5b12e42cc0d3296bd775f1176801e62d716dc0f5
westh <west.harry2004@gmail.com>
2025-11-08 19:34:43 +0000

    moved styling to styles.css

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 function makeid(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    }
-
-  const CODE_EDITOR_BASE_URL = "https://code-editor.ada-cs.org";
-  const iFrame = document.getElementById("code-editor");
-  const iFrameContainer = document.getElementById("ide"); // Change styles of this
-  let targetDomainSource;
-  let targetDomainOrigin;
-  let uid = makeid(10);
-  iFrame.setAttribute("src", CODE_EDITOR_BASE_URL + "/#" + uid);
-
-  function setupFrame() {
-    sendMessage({
-      type: "initialise",
-      language: "python",
-      code: "print('')",
-      fullscreen: true
-    });
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
+  return result;
+}
 
-  function sendMessage(obj) {
+window.addEventListener("message", handleReceive);
+
+const CODE_EDITOR_BASE_URL = "http://localhost:3000";
+const iFrame = document.getElementById("code-editor");
+iFrame.onload = setupFrame;
+const iFrameContainer = document.getElementById("ide"); // Change styles of this
+let uid = makeid(10);
+iFrame.setAttribute("src", CODE_EDITOR_BASE_URL + "/#" + uid);
+
+function setupFrame() {
+  sendMessage({
+    type: "initialise",
+    language: "python",
+    code: "print('hello world!')",
+    fullscreen: true,
+    uid: uid
+  });
+}
+
+function sendMessage(obj) {
+    if (iFrame instanceof HTMLElement && iFrame.contentWindow) {
       console.log(iFrame.src)
-      console.log("Sending message:", obj);
-      if (iFrame instanceof HTMLElement && iFrame.contentWindow) {
-          iFrame.contentWindow.postMessage(obj, iFrame.src);
-      } else if (undefined !== targetDomainSource && undefined !== targetDomainOrigin) {
-          targetDomainSource.postMessage(obj, targetDomainOrigin);
-      } else {
-          // This should only happen if undefined foreignDomain and no message is received yet
-          console.log("If foreignDomain is undefined, this can only reply to messages (i.e. can send only after the first message has been received)");
-      }
-      console.log("Initialise message sent");
+      console.log(obj)
+      iFrame.contentWindow.postMessage(obj, iFrame.src);
+    } else {
+      console.log("iFrame not defined.");
     }
-
-  function handleReceive(e) {
-      console.log("Message received:", e);
+    console.log("Initialise message sent");
   }
 
-  window.addEventListener("message", handleReceive());
+function handleReceive(e) {
+  if (!iFrame || e.source !== iFrame.contentWindow) {
+    return;
+  }
+  console.log("Message received:", e);
+}
